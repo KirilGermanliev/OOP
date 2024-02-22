353b2d4ee7f321ef238197d17db1f8ebf0155af4
KirilGermanliev <146926954+KirilGermanliev@users.noreply.github.com>
2024-02-22 17:22:36 +0200

    Update Event.cpp

--- Event.cpp
+++ Event.cpp
@@ -9,7 +9,7 @@ struct Time {
 	long long turnToNum() const {
 		return(hour * 60 * 60 + minute * 60 + second);
 	}
-	Time sum(Time& t) const{
+	Time sum(Time& t) const {
 		return{ t.hour + hour, t.minute + minute, t.second + second };
 	}
 	Time format() {
@@ -28,7 +28,8 @@ struct Event {
 	Time end_time;
 	Time get_duration() const {
 		if (end_time.turnToNum() >= start_time.turnToNum()) {
-			return{ end_time.hour - start_time.hour, end_time.minute - start_time.minute, end_time.second - start_time.second };
+			long long diff = end_time.turnToNum() - start_time.turnToNum();
+			return{ diff/3600, (diff/60)%60, diff%60 };
 		}
 		else {
 			throw("Invalid start and end time");
@@ -36,7 +37,10 @@ struct Event {
 	}
 };
 void addEvent(Event calendar[], int k) {
+	cout << "Type title:";
+	cin.ignore();
 	cin.getline(calendar[k].title, 129);
+	cout << "Type organizer:";
 	cin.getline(calendar[k].organizer, 129);
 	cin >> calendar[k].start_time.hour >> calendar[k].start_time.minute >> calendar[k].start_time.second;
 	cin >> calendar[k].end_time.hour >> calendar[k].end_time.minute >> calendar[k].end_time.second;
@@ -46,20 +50,23 @@ void createTable(Event calendar[], int n, Time& full) {
 	for (int i = 0; i < n; i++) {
 		addEvent(calendar, i);
 	}
-	Time duration;
+	Time duration = { 0,0,0 };
 	for (int i = 0; i < n; i++) {
 		duration = calendar[i].get_duration().sum(duration);
+		cout << duration.hour << ":" << duration.minute << ":" << duration.second << endl;
 	}
+	cout << duration.hour << ":" << duration.minute << ":" << duration.second << endl;
 	full = duration.format();
 }
 int main() {
 	int n;
-	cout << "Type number of events";
+	cout << "Type number of events:";
 	cin >> n;
+	cin.ignore();
 	assert(n < 101);
 	Event timetable[100];
 	Time fullDuration;
-	
-
+	createTable(timetable, n, fullDuration);
+	cout << fullDuration.hour << ":" << fullDuration.minute << ":" << fullDuration.second << endl;
 	return 0;
 }
